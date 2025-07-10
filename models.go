2a215b589f5664ed951b691df97474d9fef7bf47
Ayush Agrawal <ayushagra@google.com>
2025-07-10 11:51:13 -0700

    feat: Restore presubmit_piper_to_github Copybara workflow, that was disabled in cl/780166711. Submit to revert change after that CL is submitted.
    PiperOrigin-RevId: 781622076

--- models.go
+++ models.go
@@ -4925,6 +4925,7 @@ func (m Models) generateContentStream(ctx context.Context, model string, content
 	}
 	delete(body, "_url")
 	delete(body, "config")
+
 	err = sendStreamRequest(ctx, m.apiClient, path, http.MethodPost, body, httpOptions, &rs)
 	if err != nil {
 		return yieldErrorAndEndIterator[GenerateContentResponse](err)

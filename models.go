7f8e81576cb8107dde4c661444dc44b3de55a175
Mark Daoust <markdaoust@google.com>
2026-08-11 12:45:37 -0700

    chore: Part.media_processing
    PiperOrigin-RevId: 962940780

--- models.go
+++ models.go
@@ -3522,6 +3522,11 @@ func partToMldev(fromObject map[string]any, parentObject map[string]any, rootObj
 		InternalSetValueByPath(toObject, []string{"audioTranscription"}, fromAudioTranscription)
 	}
 
+	fromMediaProcessing := InternalGetValueByPath(fromObject, []string{"mediaProcessing"})
+	if fromMediaProcessing != nil {
+		InternalSetValueByPath(toObject, []string{"mediaProcessing"}, fromMediaProcessing)
+	}
+
 	return toObject, nil
 }
 
@@ -3608,6 +3613,11 @@ func partToVertex(fromObject map[string]any, parentObject map[string]any, rootOb
 	fromAudioTranscription := InternalGetValueByPath(fromObject, []string{"audioTranscription"})
 	if fromAudioTranscription != nil {
 		InternalSetValueByPath(toObject, []string{"audioTranscription"}, fromAudioTranscription)
+	}
+
+	fromMediaProcessing := InternalGetValueByPath(fromObject, []string{"mediaProcessing"})
+	if fromMediaProcessing != nil {
+		InternalSetValueByPath(toObject, []string{"mediaProcessing"}, fromMediaProcessing)
 	}
 
 	return toObject, nil
